28ab976b7cd1ac527d950a5b9f7bcf3c71e1415e
Younhong <sigkfkadk@hanmail.net>
2016-11-10 17:25:46 +0900

    Checking file name
    Checking file name and style.

--- java code/FileReader.java
+++ java code/FileReader.java
@@ -1,68 +1,37 @@
 import java.io.*;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class FileReader {
-	
-	public static void main(String args[]) throws IOException
-	{
-		int r = 1;
-		while(r > 0){
-			
-			// Enter input filename.
-			System.out.println("Enter name of a md file name: ");
-			Scanner input = new Scanner(System.in);
-			String filename = input.nextLine();		
-			if(validInput(filename) > 0)
-			{
-				continue;
-			}
-			
-			// Enter output filename.
-			System.out.println("Enter name of an output file: ");
-			String filename_out = input.nextLine();		
-			if(validInput(filename) > 0)
-			{
-				continue;
-			}
-			
-			// Enter the style of the file.
-			System.out.println("Enter style of the output file: ");
-			String style = input.nextLine();		
-			if(validInput(filename) > 0)
-			{
-				continue;
-			}
-			
+	public static void main(String args[]) throws IOException {
+		Scanner input = new Scanner(System.in);
+
+		String input_file = args[0];
+		String output_file = args[1];
+		String style = args[2];
+
+		while (!input_file.substring(input_file.length()-3, input_file.length()).equals(".md")) {
+			System.out.println("Not valid input file name");
+			System.out.println("Enter input file name: ");
+			input_file = input.nextLine();
 		}
-		
+
+		System.out.println();
+		while (!output_file.substring(output_file.length()-5, output_file.length()).equals(".html")) {
+			System.out.println("Not valid output file name");
+			System.out.println("Enter output file name: ");
+			output_file = input.nextLine();
+		}
+
+		System.out.println();
+		while (!style.equalsIgnoreCase("fancy") && !style.equals("plain") && !style.equals("slide")) {
+			System.out.println("Not valid style name");
+			System.out.println("Enter style name: ");
+			style = input.nextLine();
+		}
+
+		// String help = "help";
+		// check validity of file name and style
+
 	}
-	
-	static int validInput(String s)
-	{
-		char dotsplit = '.';
-		int result = 0;
-		// filename input Ã³¸®
-		if(s.isEmpty())
-		{
-			System.out.println("File name has not been entered. Enter the file name");
-			result = 1;
-		}
-		// filename that does not contain its type:
-		else if(s.indexOf(dotsplit)==0) // invalid because it does not have filename
-		{
-			System.out.println("Inappropriate file name! Please type a filename in following format:");
-			System.out.println("file_name.file_type");
-			result = 2;
-		}
-		else if(s.indexOf(dotsplit)==-1) // does not contain file type
-		{
-			System.out.println("please contain the file type.");
-			result = 3;
-		}
-		
-		return result;
-	}
-	
 }

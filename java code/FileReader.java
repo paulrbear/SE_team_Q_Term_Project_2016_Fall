8c819328b5f2906c47ac62eabcf66302f89f7c3b
Younhong <sigkfkadk@hanmail.net>
2016-11-10 20:52:44 +0900

    file name reading
    file name reading

--- java code/FileReader.java
+++ java code/FileReader.java
@@ -1,37 +1,50 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class FileReader {
 	public static void main(String args[]) throws IOException {
+		// command line typing format: java FileReader (input_file) (output_file) (style)
 		Scanner input = new Scanner(System.in);
-
 		String input_file = args[0];
 		String output_file = args[1];
 		String style = args[2];
+
+		if (input_file.equalsIgnoreCase("help") || output_file.equalsIgnoreCase("help") || style.equalsIgnoreCase("help")) {
+			System.out.println("help message");
+			System.exit(1);
+		}
 
 		while (!input_file.substring(input_file.length()-3, input_file.length()).equals(".md")) {
 			System.out.println("Not valid input file name");
 			System.out.println("Enter input file name: ");
 			input_file = input.nextLine();
+			if (input_file.equalsIgnoreCase("help")) {
+				System.out.println("help message");
+				System.exit(1);
+			}
 		}
 
-		System.out.println();
 		while (!output_file.substring(output_file.length()-5, output_file.length()).equals(".html")) {
 			System.out.println("Not valid output file name");
 			System.out.println("Enter output file name: ");
 			output_file = input.nextLine();
+			if (output_file.equalsIgnoreCase("help")) {
+				System.out.println("help message");
+				System.exit(1);
+			}
 		}
 
-		System.out.println();
-		while (!style.equalsIgnoreCase("fancy") && !style.equals("plain") && !style.equals("slide")) {
+		while (!style.equalsIgnoreCase("fancy") && !style.equalsIgnoreCase("plain") && !style.equalsIgnoreCase("slide")) {
 			System.out.println("Not valid style name");
 			System.out.println("Enter style name: ");
 			style = input.nextLine();
+			if (style.equalsIgnoreCase("help")) {
+				System.out.println("help message");
+				System.exit(1);
+			}
 		}
 
-		// String help = "help";
-		// check validity of file name and style
-
+		// read input file and output file
+		// check whether input file exists or not
 	}
 }

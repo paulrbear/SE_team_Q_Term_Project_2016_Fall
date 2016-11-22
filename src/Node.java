2971603e77afdc00ee1647d9d100d9e2de6ad0c1
boomkim <bhk3177@gmail.com>
2016-11-22 18:04:27 +0900

    nodeType: private to public
    this need to be public

--- src/Node.java
+++ src/Node.java
@@ -9,7 +9,7 @@ public class Node {
 	
 	//ATTRIBUTES!!!!!
 	static ArrayList<Token> tokens; // token list
-	private NodeType nodeType;	// node의 type
+	public NodeType nodeType;	// node의 type
 	public String tempS; // divideToken method의 parameter로 들어가서 update될 string이다.
 	
 	public void setNodeType(NodeType nt)

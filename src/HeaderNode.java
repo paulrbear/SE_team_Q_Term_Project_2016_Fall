30cd386edc2c157fb10c8ddba37c7b6d2e11b495
boomkim <bhk3177@gmail.com>
2016-11-24 23:51:17 +0900

    HeaderNode

--- src/HeaderNode.java
+++ src/HeaderNode.java
@@ -1,4 +1,14 @@
 
 class HeaderNode extends Node{
+	public enum NodeStyle{
+		H1,H2,H3,H4,H5,H6;
+	}
+	public NodeStyle nodeStyle;
+	
+	public HeaderNode(String str,NodeStyle style){
+		nodeString = str;
+		nodeStyle = style;
+		nodeParse();
+	}
 
 }

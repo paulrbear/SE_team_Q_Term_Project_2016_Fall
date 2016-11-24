d7d01f67359dafc88d505efa7e9d4495165ff881
boomkim <bhk3177@gmail.com>
2016-11-25 02:35:27 +0900

    deleted header token

--- src/HeaderToken.java
+++
@@ -1,9 +0,0 @@
-class HeaderToken extends Token{
-	// return type header
-	// 8 types of header
-	// #, ##, ###, ####, #####, ######
-	// =======, -------
-	public void accept(Visitor v) {
-		v.visitToken(this);
-	}
-}

348a2c0b56f8551910ccae6b8dc1eb9ce99a80f3
paulrbear <paulrbear@gmail.com>
2016-12-04 20:30:48 +0900

    Minor Changes in Backslash & added Token to Visitor
    Two solutions for handling Plain, Bold, Italics:
    1. have separate Classes
    2. have a single Class and define TokenStyle
    So I made for both Classes

--- src/BackslashEscape.java
+++ src/BackslashEscape.java
@@ -1,6 +1,24 @@
 public class BackslashEscape {
 
-	public String BlackslashEscape(String checkString){
+	public String BackslashEscape(String checkString){
+		//Backslash			
+			/*
+			/////////////////////////////////////////
+
+			Markdown provides backslash escapes for the following characters:
+			\ backslash
+			бо backtick
+			* asterisk
+			_ underscore
+			{} curly braces
+			[] square brackets
+			() parentheses
+			# hash mark
+			. dot
+			! exclamation mark
+			
+			////////////////////////////////////////
+			*/
 		checkString.replaceAll("(\\)(\\*)","(\\*)");
 		checkString.replaceAll("(\\)(\\)","(\\)");
 		checkString.replaceAll("(\\)(\\')","(\\')");
@@ -14,27 +32,14 @@ public class BackslashEscape {
 		checkString.replaceAll("(\\)(\\#)","(\\#)");
 		checkString.replaceAll("(\\)(\\.)","(\\.)");
 		checkString.replaceAll("(\\)(\\!)","(\\!)");
+		
+		//Char that are not quite BE but needs replacing
+		// "<" -> &lt, "&" -> &amp
+		checkString.replaceAll("\\<", "\\&lt");
+		checkString.replaceAll("\\&", "\\&amp");
+		
+		//return result
 		return checkString;
 	}
 }
 
-
-/*
-"<" -> &lt, "&" -> &amp 
-
-
-\* literal asterisks \*
-Markdown provides backslash escapes for the following characters:
-\ backslash
-бо backtick
-* asterisk
-_ underscore
-{} curly braces
-[] square brackets
-() parentheses
-# hash mark
-. dot
-! exclamation mark
-
-
-*/

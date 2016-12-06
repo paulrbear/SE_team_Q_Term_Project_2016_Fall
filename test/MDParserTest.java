8d00cd46937932b6fc5e21213b9e8e9d5b01ff95
paulrbear <paulrbear@gmail.com>
2016-12-06 20:17:16 +0900

    test mock files

---
+++ test/MDParserTest.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MDParserTest {
+
+	@Test
+	public void comparePNTest(){
+		MDParser mdparser = new MDParser();
+	}
+	@Test
+	public void isStartTest(){
+		MDParser mdparser = new MDParser();
+	}
+	@Test
+	public void isEndTest(){
+		MDParser mdparser = new MDParser();
+	}
+	@Test
+	public void HeaderNumTest(){
+		MDParser mdparser = new MDParser();
+	}
+	@Test
+	public void initializeAllTest(){
+		MDParser mdparser = new MDParser();
+	}
+	@Test
+	public void parserTest(){
+		MDParser mdparser = new MDParser();
+	}
+	@Test
+	public void getDocTest(){
+		MDParser mdparser = new MDParser();
+	}
+	
+	
+}
+/*
+public class Calculator {
+  public int evaluate(String expression) {
+    int sum = 0;
+    for (String summand: expression.split("\\+"))
+      sum += Integer.valueOf(summand);
+    return sum;
+  }
+}
+
+public class CalculatorTest {
+  @Test
+  public void evaluatesExpression() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+}
+
+*/
